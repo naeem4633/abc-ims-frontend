53964f16354ac8a899f685cd999984b66fb721cf
naeem4633 <91604353+naeem4633@users.noreply.github.com>
2024-12-19 11:59:37 +0000

    created Login and Sign up pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import LandingPage from './pages/LandingPage';
+import Signup from './pages/Signup';
+// import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div className="app">
+        <div className="app-body">
+          <Routes>
+            <Route path="/" element={<LandingPage/>} />
+            <Route path="/signup" element={<Signup/>} />
+            {/* <Route path="/login" element={<Login/>} /> */}
+            {/* <Route path="*" element={<ErrorPage/>} /> */}
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
